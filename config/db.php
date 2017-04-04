77eb3aebfebf4d101927876a4bfff1f85fe10fea
erkin <bokushev@gmail.com>
2017-04-04 23:09:31 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sample',
+    'username' => 'erkin',
+    'password' => 'Qwerty123',
+    'charset' => 'utf8',
+];
