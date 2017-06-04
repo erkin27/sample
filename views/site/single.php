fd45bdb867a22473466ccc0295be68b7d154f777
erkin <bokushev@gmail.com>
2017-06-04 17:28:13 +0300

    ckfinder, ckeditor

--- views/site/single.php
+++ views/site/single.php
@@ -1,6 +1,7 @@
 <!--main content start-->
 <?php
 use yii\helpers\Url;
+
 ?>
 <div class="main-content">
     <div class="container">
@@ -8,27 +9,32 @@ use yii\helpers\Url;
             <div class="col-md-8">
                 <article class="post">
                     <div class="post-thumb">
-                        <a href="<?=Url::toRoute(['/site/view', 'id' => $article->id])?>"><img src="<?=$article->getImage();?>" alt=""></a>
+                        <a href="<?= Url::toRoute(['/site/view', 'id' => $article->id]) ?> "><img
+                                    src="<?= $article->getImage(); ?>" alt=""></a>
                     </div>
                     <div class="post-content">
                         <header class="entry-header text-center text-uppercase">
-                            <h6><a href="<?=Url::toRoute(['/site/category', 'id'=>$article->category->id])?>"><?=$article->category->title;?></a></h6>
+                            <h6>
+                                <a href="<?= Url::toRoute(['/site/category', 'id' => $article->category->id]) ?>"><?= $article->category->title; ?></a>
+                            </h6>
 
-                            <h1 class="entry-title"><a href="<?=Url::toRoute(['/site/view', 'id' => $article->id])?>"><?=$article->title;?></a></h1>
+                            <h1 class="entry-title"><a
+                                        href="<?= Url::toRoute(['/site/view', 'id' => $article->id]) ?>"><?= $article->title; ?></a>
+                            </h1>
 
 
                         </header>
                         <div class="entry-content">
-                          <?=$article->content;?>
+                            <?= $article->content; ?>
                         </div>
                         <div class="decoration">
-                            <a href="#" class="btn btn-default">Decoration</a>
+                            <a href="#" class="btn btn-ddddefault">Decoration</a>
                             <a href="#" class="btn btn-default">Decoration</a>
                         </div>
 
                         <div class="social-share">
 							<span
-                                class="social-share-title pull-left text-capitalize">By Rubel On <?=$article->getDate();?></span>
+                                    class="social-share-title pull-left text-capitalize">By Rubel On <?= $article->getDate(); ?></span>
                             <ul class="text-center pull-right">
                                 <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
                                 <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
@@ -39,24 +45,17 @@ use yii\helpers\Url;
                         </div>
                     </div>
                 </article>
-                <div class="leave-comment"><!--leave comment-->
-                    <h4>Leave a reply</h4>
-                    <form class="form-horizontal contact-form" role="form" method="post" action="#">
-                        <div class="form-group">
-                            <div class="col-md-12">
-										<textarea class="form-control" rows="6" name="message"
-                                                  placeholder="Write Massage"></textarea>
-                            </div>
-                        </div>
-                        <a href="#" class="btn send-btn">Post Comment</a>
-                    </form>
-                </div><!--end leave comment-->
+            <?= $this->render('/partials/comment', [
+                'article' => $article,
+                'comments' => $comments,
+                'commentForm' => $commentForm
+            ])?>
             </div>
-        <?= $this->render('/partials/sidebar', [
-            'popular' => $popular,
-            'recent' => $recent,
-            'categories' => $categories
-        ]); ?>
+            <?= $this->render('/partials/sidebar', [
+                'popular' => $popular,
+                'recent' => $recent,
+                'categories' => $categories
+            ]); ?>
         </div>
     </div>
 </div>
